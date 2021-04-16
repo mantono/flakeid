ba40baef912095c5efdf5b3f07dcb50ed1df5334
Anton Österberg <anton.osterberg@gmail.com>
2021-04-16 19:16:19 +0200

    Use MAC address

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
+use data_encoding::BASE64;
+use mac_address::{get_mac_address, MacAddress, MacAddressError};
 use std::{
+    convert::TryFrom,
     fmt::{Display, Write},
+    io::Read,
     time::{Duration, Instant, SystemTime},
     u128,
 };
-
-use data_encoding::BASE64;
 
 #[derive(Debug, Eq, Ord, Clone, Copy)]
 pub struct Flake(u128);
@@ -16,39 +18,64 @@ impl Display for Flake {
 }
 
 pub struct FlakeGen {
-    node_id: u128,
+    mac_addr: u64,
     timestamp: u128,
     seq: u16,
 }
 
 const TIME_BITS: usize = 64;
-const NODE_BITS: usize = 48;
+const ADDR_BITS: usize = 48;
 const SEQ_BITS: usize = 16;
 
+impl From<MacAddressError> for FlakeGenErr {
+    fn from(_: MacAddressError) -> Self {
+        FlakeGenErr::NoMacAddr
+    }
+}
+
 impl FlakeGen {
-    pub fn new(node_id: Option<u128>) -> FlakeGen {
-        FlakeGen {
-            node_id: node_id.unwrap_or(0),
+    pub fn new() -> Result<FlakeGen, FlakeGenErr> {
+        let mac_addr: MacAddress = get_mac_address()?.ok_or_else(|| FlakeGenErr::NoMacAddr)?;
+        let mac_addr: u64 = mac_addr
+            .bytes()
+            .iter()
+            .fold(0u64, |acc, value| (acc << 8) + (*value as u64));
+
+        let gen = FlakeGen {
+            mac_addr,
             timestamp: 0,
             seq: 0,
-        }
+        };
+        Ok(gen)
     }
 
     pub fn gen(&mut self) -> Result<Flake, FlakeErr> {
         let time: u128 = Self::time()?;
         let seq: u16 = self.sequence(time)?;
-        let value: u128 = Self::build(time, self.node_id, seq);
+        let value: u128 = Self::build(time, self.mac_addr, seq);
         Ok(Flake(value))
     }
 
-    /// |                              Time                                     |                       Node                          |       Seq       |
-    ///  00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
-    fn build(time: u128, node: u128, seq: u16) -> u128 {
+    fn build(time: u128, node: u64, seq: u16) -> u128 {
         println!("{}.{}.{}", time, node, seq);
-        let time = time << (NODE_BITS + SEQ_BITS);
-        let node = (node << (NODE_BITS + SEQ_BITS)) >> TIME_BITS;
-        println!("{}.{}.{}", time, node, seq);
-        dbg!(node ^ time ^ (seq as u128))
+        let node: u128 = node as u128;
+        let seq: u128 = seq as u128;
+
+        println!("{:#0128b}", time);
+        println!("{:#0128b}", node);
+        println!("{:#0128b}", seq);
+
+        let time = time << (ADDR_BITS + SEQ_BITS);
+        let node = node << SEQ_BITS;
+
+        println!("Shifted <<");
+        println!("{:#0128b}", time);
+        println!("{:#0128b}", node);
+        println!("{:#0128b}", seq);
+
+        let id: u128 = dbg!(node ^ time ^ seq);
+        println!("{:#0128b}", id);
+        id
     }
 
     fn time() -> Result<u128, FlakeErr> {
@@ -67,11 +94,17 @@ impl FlakeGen {
             std::cmp::Ordering::Greater => Err(FlakeErr::TimeDrift),
         };
         if let Ok(seq) = result {
-            self.seq = dbg!(seq);
-            self.timestamp = dbg!(timestamp);
+            self.seq = seq;
+            self.timestamp = timestamp;
         };
         result
     }
+}
+
+#[derive(Debug)]
+#[non_exhaustive]
+pub enum FlakeGenErr {
+    NoMacAddr,
 }
 
 impl Flake {
@@ -104,7 +137,7 @@ mod tests {
     use crate::FlakeGen;
     #[test]
     fn two_ids_are_not_same() {
-        let mut gen = FlakeGen::new(None);
+        let mut gen = FlakeGen::new().unwrap();
         let id1 = gen.gen().unwrap();
         let id2 = gen.gen().unwrap();
         println!("{} vs {}", id1, id2);
