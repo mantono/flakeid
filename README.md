fe6b51ee79e8e7099c7b848750e38868ab1174f6
Anton Österberg <anton.osterberg@protonmail.com>
2022-06-14 02:09:04 +0200

    Add README

---
+++ README.md
@@ -0,0 +1,4 @@
+# flakeid
+Rust implementation for generation of [Flake ID](https://github.com/boundary/flake).
+
+See the [examples](examples/) folder for a demonstration of how this library can be used.
--- examples/generate.rs
+++ examples/generate.rs
@@ -1,6 +1,6 @@
 use flakeid::gen::FlakeGen;
 
-pub fn main() {
+fn main() {
     let mut gen = FlakeGen::new().expect("Unable to create generator");
     let id = gen.next().expect("Unable to generate ID");
     println!("base64: {id}");
